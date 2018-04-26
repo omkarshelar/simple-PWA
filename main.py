e992a279e11665a088610ac7f93dda3d585481c8
Omkar Shelar <omkarshelar24@gmail.com>
2018-04-26 16:53:33 +0530

    Making it better

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, send_from_directory
+from flask_sslify import SSLify
+
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 @app.route('/', methods=['GET'])
 def index():
